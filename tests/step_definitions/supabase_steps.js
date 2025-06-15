4d78d81a84e3ef71e0941f5d9918434900c0269b
parsvensson <parsvensson@users.noreply.github.com>
2025-06-15 08:16:33 +0000

    Deploy from videobrowser @ 60c268df0685f8e95c951e53ab35a51b697c93c1 parsvensson/videobrowser@bc167f359d08914e02da2df82dd179640a2d621e

--- tests/step_definitions/supabase_steps.js
+++ tests/step_definitions/supabase_steps.js
@@ -3,10 +3,6 @@ const { Given } = createBdd();
 import { getBaseUrl } from '../../js/utils/testBaseUrl';
 
 Given('the user is on the application\'s main page with Supabase configured', async ({ page }) => {
-  await page.addInitScript(() => {
-    window.SUPABASE_URL = 'https://dzxzmneyogcypfaqusgi.supabase.co';
-    window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR6eHptbmV5b2djeXBmYXF1c2dpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk5MTUxMTQsImV4cCI6MjA2NTQ5MTExNH0.M59Fd-oBAGNDconMvhfZy7tWtk8XIi71us2DUI73Sek';
-  });
   await page.goto(getBaseUrl());
 });
 
